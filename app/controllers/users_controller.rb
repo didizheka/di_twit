f858cb1962c718f2d19270f04f771d1539436693
didizheka <d.zhexen@gmail.com>
2015-10-26 11:13:02 +0000

    Chapter 9 part 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Регистрация прошла успешно"
       redirect_to @user
     else
@@ -17,13 +18,26 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update 
+    if @user.update_attributes(user_params)
+      flash[:success] = " Регистрация прошла успешно"
+      redirect_to @user
+      else
+        render 'edit'
+    end
+  end
   
    private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    def signed_in_user
+      redirect_to signin_url, notice: "Необходимо войти в систему" unless signed_in?
     end
   
   
