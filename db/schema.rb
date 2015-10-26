4a9e6c9b53b335dc9d55dc446a3253b293eee1ba
didizheka <d.zhexen@gmail.com>
2015-10-26 10:38:12 +0000

    Create sessions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023093356) do
+ActiveRecord::Schema.define(version: 20151026094334) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
